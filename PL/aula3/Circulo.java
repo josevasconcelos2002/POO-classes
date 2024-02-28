8bd7195c4c3a69eea4e48d86e9d62400c9128ee1
Jose Vasconcelos <zevasconcelos6@gmail.com>
2024-02-28 16:36:49 +0000

    .

---
+++ PL/aula3/Circulo.java
@@ -0,0 +1,63 @@
+package aula3;
+
+public class Circulo {
+
+    private double x;
+    private double y;
+    private double raio;
+
+    /**
+     * Gets the value of x.
+     *
+     * @return the value of x
+     */
+    public double getX() {
+        return this.x;
+    }
+
+    /**
+     * Gets the value of the y coordinate.
+     *
+     * @return the value of the y coordinate
+     */
+    public double getY() {
+        return this.y;
+    }
+
+    /**
+     * Get the value of raio.
+     *
+     * @return the value of raio
+     */
+    public double getRaio() {
+        return this.raio;
+    }
+
+    /**
+     * Sets the value of x.
+     *
+     * @param x the new value for x
+     */
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    /**
+     * Set the value of y.
+     *
+     * @param y the new value for y
+     */
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    /**
+     * Set the value of the raio property.
+     *
+     * @param raio the new value for the raio property
+     */
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+}
---
+++ PL/aula3/Ficha3.java
@@ -0,0 +1,8 @@
+package aula3;
+
+public class Ficha3 {
+
+    public static void main(String[] args) {
+
+    }
+}
